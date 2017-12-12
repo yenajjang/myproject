8243416b3d99287ddd5297bf7205dcb0a83aa090
yena <yena@SIST215>
2017-12-12 14:26:54 +0900

    first commit

---
+++ MVCProject_2/src/com/sist/model/ContentModel.java
@@ -0,0 +1,13 @@
+package com.sist.model;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class ContentModel implements Model {
+
+	@Override
+	public String execute(HttpServletRequest req) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
